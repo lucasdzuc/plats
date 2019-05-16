f943d575cfff3959aee2ddbd87a3ee1caf5c6b81
lucasdzuc <lucas_morais_an@hotmail.com>
2019-05-16 14:02:19 -0300

    ct1

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule }              from '@angular/core';
+import { RouterModule, Routes }  from '@angular/router';
+ 
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ServicosComponent } from './servicos/servicos.component';
+import { ContatoComponent } from './contato/contato.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ModuleWithProviders } from '@angular/compiler/src/core';
+ 
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'sobre', component: SobreComponent },
+  { path: 'servicos', component: ServicosComponent },
+  { path: 'contato', component: ContatoComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: '',   redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
+ 
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule, routing } from './app-routing.module';
+
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ServicosComponent } from './servicos/servicos.component';
+import { ContatoComponent } from './contato/contato.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SobreComponent,
+    ServicosComponent,
+    ContatoComponent,
+    LoginComponent,
+    CadastroComponent,
+    PageNotFoundComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
